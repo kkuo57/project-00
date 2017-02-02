06ef096883bf7b88ff04a1f816a611e58120970a
kkuo57 <kkuo507@gmail.com>
2017-02-01 21:36:03 -0800

    added cars and got them to move independently

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+/*var raceArray = ["", "", ""];
 $(window).on("keypress", function handleKeypress(){
-  
+  for (i = 0; i < raceArray.length; i++){}
+});*/
+var playerone_count = 20;
+var playertwo_count = 20
+
+$(window).on("keyup", function handleKeyup(event){
+  console.log(event.which)
+  if (event.which === 65 ||  event.which === 83 ){
+    $(".playerone").attr("style", "margin-left:" + playerone_count + "px;");
+    playerone_count = playerone_count + 20;
+  }
+  if (event.which === 186 || event.which === 222 ){
+    $(".playertwo").attr("style", "margin-left:" + playertwo_count + "px;");
+    playertwo_count = playertwo_count + 20;  
+  }
 
 });
